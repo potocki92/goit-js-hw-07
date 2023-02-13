f2dad601bf506659bf0b09be047bdf96770abb53
Mateusz Potocki <40219157+potocki92@users.noreply.github.com>
2023-02-13 20:13:03 +0000

    *

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+
+const createItem = (item) => {
+  return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img 
+          class="gallery__image" 
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </div>
+  `
+}
+
+const items = galleryItems.map(item =>
+  createItem(item)).join("");
+gallery.innerHTML = items;
+
+gallery.addEventListener("click", e => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" alt="${e.target.alt}" width="800" height="600"/>
+`)
+
+  instance.show()
+
+  gallery.addEventListener("keyup", e => {
+    if (e.key === "Escape") {
+      instance.close()
+    }
+  })
+})
