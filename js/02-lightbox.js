b24228e906aaee0b4c93e4fac25da4e0da624f7e
Mateusz Potocki <40219157+potocki92@users.noreply.github.com>
2023-02-14 15:45:34 +0000

    wykonanie zadania drugiego "lightbox"

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+
+const createItem = (item) => {
+  return `
+      <a class="gallery__item" href="${item.original}">
+        <img 
+          class="gallery__image" 
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+  `
+}
+
+const items = galleryItems.map(item =>
+  createItem(item)).join("");
+gallery.innerHTML = items;
+
+let lightbox = new SimpleLightbox(`.gallery a`, {
+  captionDelay: 250,
+  captionsData: 'alt',
+  overlayOpacity: 0.7
+})
+lightbox.open(0)
+lightbox.next()
